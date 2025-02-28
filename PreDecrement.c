685fbb4b9a438b5c6d4b6c86af226f7b8d6d83da
SNSIMANTO <snsimanto.cse.sub@gmail.com>
2025-02-28 11:52:50 +0600

    Operator

---
+++ PreDecrement.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int x=10;
+    int y=--x; //y=9
+    printf("x=%d\n",x);//x=9
+    printf("y=%d\n",y);
+
+    return 0;
+}
---
+++ UnaryPlus.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int x=6;
+    int result = +x;
+
+    printf("The result is:%d",result);
+    return 0;
+}
