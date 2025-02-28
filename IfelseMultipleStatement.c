65495b23bb50c547ae0204fa6886a61b24121640
SNSIMANTO <snsimanto.cse.sub@gmail.com>
2025-02-28 14:50:17 +0600

    multiple statement if else

---
+++ IfelseMultipleStatement.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int time;
+    printf("Enter Time:");
+    scanf("%d",&time);
+
+    if(time==10)
+    {
+        printf("Good morning ");
+        printf("Simanto");
+    }
+    else
+    {
+        printf("Its not morning ");
+        printf("simantO.");
+    }
+}
+
